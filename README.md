fa01b92a80bf36a9375c597c768b4d926ee41f15
cel8 <alex.celotti@gmail.com>
2022-07-13 13:29:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# top_restaurant
+A restaurant landing page part of <a href="https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page">The Odin Project assignment</a>.
+
+Learning:
+- Webpack
+- HTML automation
+- Javascript
+
+Live demo <a href="https://cel8.github.io/top_restaurant/">here</a>.
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page - La bella Napoli',
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
